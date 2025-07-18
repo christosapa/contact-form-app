d0c165eac35417ba482e17935c5a957f9210681c
Christos <christos.a.panos@gmail.com>
2025-07-18 11:29:48 +1000

    style fixes

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
       <nav className="main-nav">
         <div className="container">
           <ul className="nav-links">
-            <li><a href="#home">Home</a></li>
+            <li><a href="#home" onClick={() => window.location.reload()}>Home</a></li>
             <li><a href="#services">Services</a></li>
             <li><a href="#about">About Us</a></li>
             <li><a href="#contact">Contact</a></li>
